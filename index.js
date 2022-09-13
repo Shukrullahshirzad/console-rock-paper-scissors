9ad1ab475e64a1728a89de6ec35fe262970a1e60
shukrullah shirzad <shukrullah.shirzad.779@gmail.com>
2022-09-13 12:28:17 +0430

    define a function for one round of the game

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function computerChoice(){
     return(choice[Math.floor(Math.random()*3)])
 }
 
+// one round of the game
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! Paper covers Rock"
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! Scissors cuts Paper"
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! Rock beats Scissors"
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return "You Lose! Scissors cuts Paper"
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper covers Rock"
+    }
+    if(playerSelection === "scissors" && computerSelection === "rock"){
+        return "You Lose! Rock beats Scissors"
+    }
+    else{
+        return "Draw"
+    }
+}
